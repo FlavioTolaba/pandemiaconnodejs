41eb63df83f522875fa0b0b9b811b29c1e7d2ffe
Rafael García Prieto <rafael.garcia.prieto@bbvaitspain.com>
2021-06-12 13:24:25 +0200

    feat: clean simple example

--- clase1/ejercicios/rafa/log4slack.js
+++ clase1/ejercicios/rafa/log4slack.js
@@ -1,22 +1,10 @@
-const logger = require('../../lib/logger')('ejemplo');
 const { WebClient } = require('@slack/web-api');
-const token = require('./config').log4slackToken
-
-logger.info('*****************************');
-logger.info('I N I C I A   P R O G R A M A');
-logger.info('*****************************');
-logger.trace('traceador');
-logger.debug('debug');
-logger.info('log de info');
-logger.warn('esto es un warning');
-logger.error('esto es cuando ocurre un error');
-logger.fatal('esto es cuando se esta por abortar un proceso');
-
+const token = require('../../config/tokens').log4slackToken
 const web = new WebClient(token);
 const conversationId = 'logger-slack';
 
 (async () => {
-  const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there' });
+  const res = await web.chat.postMessage({ channel: conversationId, text: 'Mensaje de prueba' });
 
   console.log('Message sent: ', res.ts);
 })();
