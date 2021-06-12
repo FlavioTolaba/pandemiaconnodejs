5a31264ad3936f087be727f50d9657a47c5e809c
Rafael García Prieto <rafael.garcia.prieto@bbvaitspain.com>
2021-06-12 12:41:59 +0200

    feat: remove tokens

--- clase1/ejercicios/rafa/log4slack.js
+++ clase1/ejercicios/rafa/log4slack.js
@@ -1,5 +1,6 @@
 const logger = require('../../lib/logger')('ejemplo');
 const { WebClient } = require('@slack/web-api');
+const token = require('./config').log4slackToken
 
 logger.info('*****************************');
 logger.info('I N I C I A   P R O G R A M A');
@@ -10,11 +11,6 @@ logger.info('log de info');
 logger.warn('esto es un warning');
 logger.error('esto es cuando ocurre un error');
 logger.fatal('esto es cuando se esta por abortar un proceso');
-
-
-// Pongo los tokens aquí pero no es su sitio
-const token = 'xoxb-2153110014865-2165534037395-ya6nyjvRYcikandsAHCxajrA';
-// const token = process.env.SLACK_BOT_TOKEN; // Éste sería el sitio bueno
 
 const web = new WebClient(token);
 const conversationId = 'logger-slack';
--- clase1/ejercicios/rafa/log4slackclassic.js
+++ clase1/ejercicios/rafa/log4slackclassic.js
@@ -1,5 +1,6 @@
 const logger = require('../../lib/logger')('ejemplo');
 const { RTMClient, LogLevel } = require('@slack/rtm-api');
+const token = require('./config').log4slackclassicToken
 
 logger.info('*****************************');
 logger.info('I N I C I A   P R O G R A M A');
@@ -10,12 +11,6 @@ logger.info('log de info');
 logger.warn('esto es un warning');
 logger.error('esto es cuando ocurre un error');
 logger.fatal('esto es cuando se esta por abortar un proceso');
-
-
-// Pongo los tokens aquí pero no es su sitio
-
-const token = 'xoxb-2153110014865-2177927982481-r7Q9r9nTe39Z7rzFET4EpGC6'; // Log4SlackClassic (formato clásico para usar RTM - Real Time Messaging)
-// const token = process.env.SLACK_BOT_TOKEN; // Éste sería el sitio bueno
 
 const rtm = new RTMClient(token, {
   logLevel: LogLevel.DEBUG
