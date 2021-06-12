69995ef6d4d8c8622cfcdbcdaa160af264e4de79
Rafael García Prieto <rafael.garcia.prieto@bbvaitspain.com>
2021-06-12 13:24:05 +0200

    feat: config log4js-node/slack

--- clase1/lib/logger.js
+++ clase1/lib/logger.js
@@ -1,12 +1,19 @@
 const log4js = require('log4js')
+const token = require('../config/tokens').log4slackToken
 
 log4js.configure({
     appenders: {
         archivo: { type: 'file', filename: '/var/log/clasearch' },
-        consola: { type: 'stdout' }
+        consola: { type: 'stdout' },
+        alerts: {
+            type: '@log4js-node/slack',
+            token,
+            channel_id: 'logger-slack',
+            username: 'log4slack'
+        }
     },
     categories : { default: {
-        appenders: ['archivo', 'consola'], level: 'trace'
+        appenders: ['archivo', 'consola', 'alerts'], level: 'trace'
     }}
 })
 
